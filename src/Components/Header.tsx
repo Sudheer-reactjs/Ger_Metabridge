1e216cdff7a6173616cbd9b47d2ab414eb5f27a4
Sudheer Kumar <j.sokoli@elogic.com.ua>
2025-10-06 17:17:35 +0530

    fonts

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,7 +2,7 @@ import Logo from "../Assets/logo.png"
 import ContactButton from "./ContactButton";
 const Header = () => {
     return (
-       <header className="fixed top-0 left-0 right-0 w-full z-50 py-3 bg-[#d9d9d9]/10 backdrop-blur-md">
+       <header className="fixed top-0 left-0 right-0 w-full z-50 py-3 bg-[#d9d9d9]/10 backdrop-blur-[4px]">
                 <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 xl:px-14">
                     <div className="flex justify-between items-center">
                         <div className="flex items-center space-x-8">
@@ -18,7 +18,7 @@ const Header = () => {
                     </div>
                 </div>
         </header>
-    );
+    ); 
 }
 
 export default Header;
