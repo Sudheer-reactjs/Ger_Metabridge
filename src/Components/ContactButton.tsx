9dc6aad88d692ea981fd6a0bf61c37c4de0cc223
Sudheer Kumar <j.sokoli@elogic.com.ua>
2025-10-07 18:08:50 +0530

    button

--- src/Components/ContactButton.tsx
+++ src/Components/ContactButton.tsx
@@ -4,7 +4,7 @@ import { ArrowRight } from "lucide-react";
 const ContactButton = () => {
     return (
         <a href="#" data-property-1="Default" className="contact-button button-hover pl-[30px] pr-1 py-1 relative bg-black rounded-[99px] border-[1px] border-solid border-transparent inline-flex justify-center items-center gap-2.5 overflow-hidden">
-            <div className="hover-bg w-[100px] h-[52px] left-[29px] top-[50px] absolute bg-[#009aff] rounded-full blur-[25px]" />
+            <div className="hover-bg w-[100px] h-[52px] left-[29px] top-[49px] absolute bg-[#009aff] rounded-full blur-[25px]" />
             <div className="justify-start text-white text-sm text text-light">Contact Us</div>
             <div className="contact-button p-[10px] bg-black/25 rounded-[99px] border-[1px] border-solid border-transparent flex justify-center items-center gap-2.5 icon ">
                 <ArrowRight strokeWidth={1.5} className="w-5 h-5 text-white" />
