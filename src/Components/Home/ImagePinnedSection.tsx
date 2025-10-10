5e8615134b72fe319c1cb964d6ceb718f6bae7a6
Sudheer Kumar <j.sokoli@elogic.com.ua>
2025-10-10 15:07:03 +0530

    pin

--- src/Components/Home/ImagePinnedSection.tsx
+++ src/Components/Home/ImagePinnedSection.tsx
@@ -1,22 +1,15 @@
 import { motion, useScroll, useTransform } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
+import { useRef } from "react";
 
 import LeftImage from "../../Assets/image-left.jpg";
 import CenterImage from "../../Assets/image-center.png";
 import RightImage from "../../Assets/image-right.jpg";
 import PinLogo from "../../Assets/pin-logo.png";
+import ContactButton from "../ContactButton";
+
 
 const ImagePinnedSection = () => {
   const ref = useRef<HTMLDivElement>(null);
-  const [isMobile, setIsMobile] = useState(false);
-
-  // Detect mobile screen width
-  useEffect(() => {
-    const checkScreen = () => setIsMobile(window.innerWidth < 768);
-    checkScreen();
-    window.addEventListener("resize", checkScreen);
-    return () => window.removeEventListener("resize", checkScreen);
-  }, []);
 
   const { scrollYProgress } = useScroll({
     target: ref,
@@ -39,8 +32,8 @@ const ImagePinnedSection = () => {
   const centerOpacity = useTransform(scrollYProgress, [0.6, 0.95], [1, 0]);
 
   // --- Text Animations ---
-  const textOpacity = useTransform(scrollYProgress, [0.6, 0.8], [0, 1]);
-  const textY = useTransform(scrollYProgress, [0.6, 0.8], [100, 0]); // slide up from below for all screens
+  const textOpacity = useTransform(scrollYProgress, [0.7, 0.9], [0, 1]);
+  const textY = useTransform(scrollYProgress, [0.7, 0.9], [600, 0]); // slide up from below for all screens
 
   return (
     <section ref={ref} className="relative h-[350vh]">
@@ -91,7 +84,7 @@ const ImagePinnedSection = () => {
             scale: circleScale,
             opacity: circleOpacity,
           }}
-          className="absolute w-14 h-28 md:w-28 md:h-48 bg-black rounded-[60px] z-20 mt-[77px]"
+          className="absolute w-14 h-28 md:w-28 md:h-48 bg-[#0b1016] rounded-[60px] z-20 mt-[77px]"
         />
 
         {/* Text */}
@@ -100,24 +93,28 @@ const ImagePinnedSection = () => {
             opacity: textOpacity,
             y: textY,
           }}
-          className="absolute z-30 w-full flex items-center justify-center top-1/2 -translate-y-1/2"
+          className="absolute z-30 w-full flex items-center justify-center top-[35%] md:top-1/2 -translate-y-1/2 mt-[-77px]"
         >
           <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 xl:px-14 ">
-          <div className="grid grid-cols-1 md:grid-cols-[_1fr_1fr] gap-2 md:gap-6 ">
+          <div className="grid grid-cols-1 items-center md:grid-cols-[_1.3fr_1fr] gap-7 md:gap-6 ">
             <div>
-          <h2 className="text-2xl md:text-4xl font-bold mb-2 md:mb-4">
-            Transform Your Vision
+          <h2 className="text-center md:text-left self-stretch justify-start text-[#fff] text-[28px] md:text-[40px] capitalize">
+            Push your budget, not your limits
           </h2>
-          <p className="text-base md:text-lg text-gray-600 leading-relaxed">
-            Experience the power of innovation blending with creativity.
-            As the visuals fade, ideas take form.
+          <p className="text-center md:text-left  self-stretch justify-start text-[#c0d5df] text-sm md:text-lg font-normal leading-[24px] md:leading-[30px]">
+           Metabridge is built by advertisers for advertisers: flexible, fast, without unnecessary bureaucracy. It allows you to scale without ever standing still.
           </p>
+          <div
+            className="flex items-start space-x-4 justify-center md:justify-start mt-6 md:mt-7 "
+          >
+            <ContactButton />
+          </div>
           </div>
           <div>
              <img
               src={PinLogo}
               alt="Pin Logo"
-              className="w-full h-full object-cover max-w-40"
+              className="w-full m-auto  object-cover max-w-[200px] md:max-w-[342px]"
             />
           </div>
           </div>
