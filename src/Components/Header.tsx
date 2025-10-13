e7b48a953e035cefe15f24eb2211b2994e400bf1
Sudheer Kumar <j.sokoli@elogic.com.ua>
2025-10-13 12:36:02 +0530

    glass effect

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,22 +2,33 @@ import Logo from "../Assets/logo.png"
 import ContactButton from "./ContactButton";
 const Header = () => {
     return (
-        <header className="fixed top-0 left-0 right-0 w-full z-50 py-3 bg-[#d9d9d9]/10 backdrop-blur-[4px]">
-            <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 xl:px-14">
-                <div className="flex justify-between items-center">
-                    <div className="flex items-center space-x-8">
-                        <a href="/">
-                            <img className="max-w-[100px]" src={Logo} alt="" />
-                        </a>
+        <header className="fixed top-0 left-0 right-0 w-full z-50">
+            <div className="card  py-3">
+                <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 xl:px-14 relative z-10">
+                    <div className="flex justify-between items-center">
+                        <div className="flex items-center space-x-8">
+                            <a href="/">
+                                <img className="max-w-[100px]" src={Logo} alt="" />
+                            </a>
+                        </div>
+                        <div className="flex items-center space-x-4">
+                            <ContactButton />
+                        </div>
                     </div>
-
-                    <div className="flex items-center space-x-4">
-                        <ContactButton />
-                    </div>
-
                 </div>
+                <svg style={{ display: 'none' }}>
+                    <filter id="displacementFilter">
+                        <feTurbulence type="turbulence"
+                            baseFrequency="0.01"
+                            numOctaves="2"
+                            result="turbulence" />
+                        <feDisplacementMap in="SourceGraphic"
+                            in2="turbulence"
+                            scale="10" xChannelSelector="R" yChannelSelector="G" />
+                    </filter>
+                </svg>
             </div>
-        </header>
+        </header> 
     );
 }
 
