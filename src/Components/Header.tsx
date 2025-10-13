0f2147f793caedc547ac76c650f69c0161ed447e
Sudheer Kumar <j.sokoli@elogic.com.ua>
2025-10-13 13:26:17 +0530

    drop

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,7 +3,7 @@ import ContactButton from "./ContactButton";
 const Header = () => {
     return (
         <header className="fixed top-0 left-0 right-0 w-full z-50">
-            <div className="card  py-3">
+            <div className="header-card  py-3">
                 <div className="max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8 xl:px-14 relative z-10">
                     <div className="flex justify-between items-center">
                         <div className="flex items-center space-x-8">
